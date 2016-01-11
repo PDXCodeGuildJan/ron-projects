f35c1bdafd07fb6970e5f83249151a05c0229863
Ron <ronshafii2@gmail.com>
2016-01-11 15:28:49 -0800

    added sequence routine

--- practiceStrings.py
+++ practiceStrings.py
@@ -5,10 +5,18 @@ def main():
 	print(myString[4:7])
 	#add string for middle index algorithm
 	middle_characters("elephant")
+	stringSequence("oregon")
 
 #determine middle index of a string
 def middle_characters(the_string):
 	middle = (len(the_string) // 2)
 	print(the_string[middle-1:middle+2])
+
+#write a function that expects an input of a sequence and prints the sequence
+def	stringSequence(string):
+	print(string)
+	for x in string:
+		print(x)
+	
 
 main()
