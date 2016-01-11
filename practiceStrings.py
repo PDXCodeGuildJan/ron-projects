e90b001364d28d8330accb35880e0c4aa97e5b07
Ron <ronshafii2@gmail.com>
2016-01-11 14:43:37 -0800

    added comments

--- practiceStrings.py
+++ practiceStrings.py
@@ -3,8 +3,10 @@ def main():
 	print(myString[1])
 	print(myString[-3])
 	print(myString[4:7])
+	#add string for middle index algorithm
 	middle_characters("elephant")
 
+#determine middle index of a string
 def middle_characters(the_string):
 	middle = (len(the_string) // 2)
 	print(the_string[middle-1:middle+2])
