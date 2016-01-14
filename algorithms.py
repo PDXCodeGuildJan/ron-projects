13e83340799cb5392ecdae12288155c53812d18d
Ron <ronshafii2@gmail.com>
2016-01-13 16:22:54 -0800

    initial list sort commit

---
+++ algorithms.py
@@ -0,0 +1,52 @@
+def selection_sort(myList):
+
+
+#####################
+#STEP 1
+####################
+#starting at the start of the unsorted list
+startingIndex = 0
+while startingIndex  < len(myList):
+
+
+	####################
+	#STEP 2
+	####################
+	#locate smallest number in unsorted list
+	
+
+
+
+	####################
+	#STEP 3
+	####################
+	#swap smallest number with first number in unsorted list
+
+
+
+
+	####################
+	#STEP 4
+	####################
+	#increase starting index by 1 of unsorted list
+	startingIndex += 1
+
+
+
+
+###################
+#STEP 5
+#####################
+#repeat until unsorted list is empty
+
+
+
+
+
+# 	write an implementation of selection sort in this function
+
+
+def main():
+	selection_sort([5, 8, 1, 0, 3])
+
+main()
