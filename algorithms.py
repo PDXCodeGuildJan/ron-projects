d7177f95893d1969dba73b4b8f4e7629fb502fbc
Ron <ronshafii2@gmail.com>
2016-01-14 13:46:45 -0800

    added sort and swap function

--- algorithms.py
+++ algorithms.py
@@ -1,52 +1,62 @@
 def selection_sort(myList):
-
-
-#####################
-#STEP 1
-####################
-#starting at the start of the unsorted list
-startingIndex = 0
-while startingIndex  < len(myList):
-
-
+	#####################
+	#STEP 1
 	####################
-	#STEP 2
-	####################
-	#locate smallest number in unsorted list
-	
+	# Begin at the start of the unsorted list
+	startingIndex = 0
+		
+	while startingIndex  < len(myList):
+
+
+		####################
+		#STEP 2
+		####################
+		# look at every item in unsorted list (loop through list)
+
+		# then locate smallest number in unsorted list
+		indexSmallest = startingIndex
+
+		for currentIndex, value in enumerate(myList[startingIndex:]):
+			if myList[indexSmallest] > value:
+				indexSmallest = currentIndex
+
+
+		####################
+		#STEP 3
+		####################
+		#swap smallest number with first number in unsorted list by creating a temp item
+
+
+		temp_item = myList[startingIndex]
+		myList[startingIndex] = myList[indexSmallest]
+		myList[indexSmallest] = temp_item
 
 
 
-	####################
-	#STEP 3
-	####################
-	#swap smallest number with first number in unsorted list
+
+
+		####################
+		#STEP 4
+		####################
+		#increase starting index by 1 of unsorted list
+		startingIndex += 1
+		print(myList)
 
 
 
-
-	####################
-	#STEP 4
-	####################
-	#increase starting index by 1 of unsorted list
-	startingIndex += 1
-
+	###################
+	#STEP 5
+	#####################
+	#repeat until unsorted list is empty
+	return myList
 
 
-
-###################
-#STEP 5
-#####################
-#repeat until unsorted list is empty
-
-
-
-
-
-# 	write an implementation of selection sort in this function
 
 
 def main():
-	selection_sort([5, 8, 1, 0, 3])
+	myList = [5, 8, 1, 0, 3]
+	print(myList)
+	myList = selection_sort(myList)
+	print(myList)
 
 main()
