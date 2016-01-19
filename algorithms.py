93779db0305615b1917323e529cb35cf6ec6f1a4
Ron <ronshafii2@gmail.com>
2016-01-19 11:32:30 -0800

    working insertion_sort

--- algorithms.py
+++ algorithms.py
@@ -134,8 +134,8 @@ def insertion_sort(myList):
 		#get the current index value. we'll call it current_value
 		current_value = myList[index]
 
-		#get the adjacent value to the left of the current_value
-		adjacent = index - 1
+		#get the adjacent value to the left of the current_value. This is a temp index.
+		adjacent = index
 
 
 
@@ -145,14 +145,14 @@ def insertion_sort(myList):
 		# start of inner loop
 		# compare current_value with adjacent_value
 		# is current_value greater than adjacent_value?
-		#while adjacent > 0 and myList[adjacent - 1] > current_value:
+		while (adjacent < (myList[adjacent - 1] and adjacent > 0):
 		#while adjacent > 0 and index < myList[adjacent - 1]:
-		while adjacent >= 0:
-			if current_value < myList[adjacent]
+		#while adjacent >= 0:
+			#if current_value < myList[adjacent]
 
 
-				#swap the values
-				swap(myList, current_value, adjacent)
+			#swap the value with adjacent value
+			swap(myList, adjacent, adjacent - 1)
 
 
 
@@ -169,7 +169,7 @@ def insertion_sort(myList):
 
 
 def main():
-	myList = [58, 8, 99, 1, 56]
+	myList = ["e", "z", "l", "o", "f", "b"]
 	print(myList)
 	myList = insertion_sort(myList)
 	print(myList)
