562ad0ae94bd40b0945da2512a7e5e6ed6eccdf9
Ron <ronshafii2@gmail.com>
2016-01-24 18:11:03 -0800

    updated format

--- Towers_of_Hanoi.py
+++ Towers_of_Hanoi.py
@@ -6,24 +6,27 @@ __author__ = "Ron Shafii"
 # The starting tower with all the discs is (start), to a destination peg where it should move to (end), 
 # and the remaining spare peg (temp) which we can temporarily use to make this happen.
 
-# Let's start with 3 discs
+# disc_height will be the number of discs stacked at the beginning
+
 def move_to_tower(disc_height, start, temp, end):
 	#if disc height is 1 then move from source to destination
 	if disc_height >= 1:
 
 	    else:
 			# move all discs except the very last one from start to the temp using the end
-		move_to_tower(disc_height-1, start, temp, end)
+			move_to_tower(disc_height-1, start, temp, end)
 
 
-		# move last disc to the end
-		mmove_a_disc(start, end)
+			# move last disc to the end
+			move_a_disc(start, end)
 
-		# move all discs from temp to end using the start as needed
-		move_to_tower(disc_height-1, temp, end, start)
+			# move all discs from temp to end using the start as needed
+			move_to_tower(disc_height-1, temp, end, start)
 
 
 
 
 # create a move disc function to swap discs from tower to tower?
 def move_a_disc(start, end):
+
+move_to_tower
