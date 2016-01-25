ffa263b9b55ae26da6f1619f1965d0b993181ec9
Ron <ronshafii2@gmail.com>
2016-01-24 18:00:05 -0800

    partial working code

--- Towers_of_Hanoi.py
+++ Towers_of_Hanoi.py
@@ -1,17 +1,30 @@
 __author__ = "Ron Shafii"
 
-"""Implementaiton for the Towers of Hanoi"""
+"""Implementaiton for the Tower of Hanoi"""
 
 # The sort_tower function will accept three parameters from which peg the tower should be moved to.
-# The starting tower with all the discs is (tower1), to a desitnation peg where it should move to (tower2), 
-# and the remaining spare peg (tower3) which we can temporarily use to make this happen.
-# Let's start with 3 discs
-def move_to_tower(disc_height, tower1, tower2, tower3):
-	if disc_height == 0
+# The starting tower with all the discs is (start), to a destination peg where it should move to (end), 
+# and the remaining spare peg (temp) which we can temporarily use to make this happen.
 
-	# move disc from tower1 to tower2
+# Let's start with 3 discs
+def move_to_tower(disc_height, start, temp, end):
+	#if disc height is 1 then move from source to destination
+	if disc_height >= 1:
+
+	    else:
+	    	#move all discs except the very last one from start to the temp using the end 
+	        move_to_tower(disc_height-1, start, temp, end)
+
+
+	        # move last disc to the end
+	        move_to_tower(start, end)
+
+	        # nive all discs from temp to end using the start as needed
+	        move_to_tower(disc_height-1, temp, end, start)
+
+
 	
 
 
 # create a move_disc function to swap discs from tower to tower?
-def move_a_disc(tower1, tower2)
+def move_a_disc(start, end)
