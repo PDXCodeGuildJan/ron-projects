7034d9f0955b2edf048a44a53c1166883941ffd7
Ron <ronshafii2@gmail.com>
2016-02-16 12:40:51 -0800

    updated comments and definitions

--- mastermind/mastermind_controller.py
+++ mastermind/mastermind_controller.py
@@ -11,28 +11,49 @@ import mastermind_view
 
 
 
+
+
+
 class MasterMind:
 
 
 	def __init__(self):
 		self.model: MasterModel
 		self.view: MasterView
+		self.goal: MasterModel
+
+
+
+	def generate.goal():
+		"""create 4 randomly generated colored pegs to establish the goal of the game."""
+		goal = []
+		for x in range(4)
+			player_peg = mastermind_model.Player_Peg(random.choice(mastermind_model.MasterModel.peg_colors))
+			goal.append(player_peg)
+			return mastermind_model.MasterModel.goal #= goal
+
+
 
 
 	def play_game():
-		"""
+		"""plays the mastermind game"""
 		pass
 
 
 	def check_win():
+		"""checks to see if the player's guess exactly matches the 4 computer generated pegs """
 		pass
 
 
 	def eval_peg_color():
+		"""Evaluates whether the player's peg colors matches any of the computer's colors. 
+		Results are stored by class Guess in the model."""
 		pass
 
-
-	def eval_key_peg():
+	def eval_peg_position():
+		"""Evaluates whether any of the matched colors from eval_peg_color also match the 
+		correct peg positions and returns a black or white peg as necessary. """
 		pass
+
 
 
--- mastermind/mastermind_view.py
+++ mastermind/mastermind_view.py
@@ -7,23 +7,50 @@ __author__ = "Ron Shafii"
 class MasterView:
 
 
-	def show_rules():
+	def show_rules(self):
+		"""Displays the rules of Mastermind to the player. """
+		print("Welcome to Mastermind!")
+
+		print("Object of the Game\n")
+		print("The computer picks a sequence of 4 pegs, each one being one of any of six colors:\n")
+		print("red, green, blue, yellow, black, and white.")
+		print("The object of the game is to guess the exact positions of the colors in the -\n")
+		print("sequence in as few guesses as possible. After each guess, the computer -\n")
+		print("gives you a score of exact and partial matches. A black peg indicates an exact -\n")
+		print("match, a white peg a partial match (right color, wrong position)\n\n")
+
+		print("Rules")
+		print("1. The sequence can contain pegs of colors: red, green, blue, yellow, black, and white.\n")
+		print("2. A color can be used any number of times in the sequence.\n")
+		print("3. Each guess must consist of 4 peg colors - no blank space are allowed.\n")
+		print("4. You are must guess the computer's sequence within 10 guesses, otherwise you lose the game.\n\n")
+
+
+
+	def show_player_peg(self):
+		"""displays the list of pegs chosen by the player during the current Guess."""
+
 		pass
 
 
-	def show_player_peg():
+	def prompt_user(self):
+		"""Prompts the user to choose 4 colored pegs."""
 		pass
 
 
-	def prompt_user():
+	def win(self):
+		"""Displays to the player "They Won!" """
 		pass
 
 
-	def win():
+
+	def lose(self):
+		"""Displays to the player "Sorry, You Lose." """
 		pass
 
 
-	def show_key_peg():
+	def show_key_peg(self):
+		"""Displays a series of black or white key pegs stored in the class Guess."""
 		pass
 
 
