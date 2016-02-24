2cebd724023766965aaae6401af567273c7e1ae5
Ron <ronshafii2@gmail.com>
2016-02-24 12:42:56 -0800

    updated button functionality

--- javascript/price_list/js/pricing.js
+++ javascript/price_list/js/pricing.js
@@ -8,15 +8,41 @@ var addStockButton = document.getElementById("add-stock");
 
 addStockButton.onclick = addStock;
 
+var removeStockButton = document.getElementById("remove-stock");
+
+removeStockButton.onclick = removeStock;
+
 
 /*toggle the inStock status on the selected rows inside of inventory*/
 
 function addStock(){
-	//NOOT ALLOWED TO USE querySelectorAll()
-	console.log()
+	//NOT ALLOWED TO USE querySelectorAll()
+
+	//create an empty list
+	var checkboxes = [];
+
+
 	//find all the selected items in the HTML list
+	//grab all the items with checkboxes and put them into a list
+	var input = document.getElementsByClassName("checkbox"); 
+	//console.log(input);
+
+
+	//loop through the list of all checkboxes and select only the checked=true items
+
+	for (var i = 0; i < input.length; i++){
+
+		if(input[i].checked){
+			checkboxes.push(input[i]);
+		
+		}
+	}
+	//console.log(checkboxes);
 
 	//change their in-stock value
+	
+	//checkboxes[i].onchange = addStock;
+
 
 	//update the display? depends on previous step. not adding quantity
 
@@ -24,9 +50,34 @@ function addStock(){
 
 
 function removeStock(){
-	//use querySElectorAll()
+	//use querySelectorAll()
+
+	//create an empty list
+	var checkboxes = [];
+
+
+	//find all the selected items in the HTML list
+	//grab all the items with checkboxes and put them into a list
+
+	var input = document.querySelectorAll('.checkbox');
+
+	for (var i = 0; i < input.length; i++){
+
+		if(input[i].checked){
+			checkboxes.push(input[i]);
+		
+		}
+	}
+
+console.log(checkboxes)
+
 
 }
+
+
+
+
+
 
 /*	Add the item in the text fields to the inventory
 	list, which is in the table body (id="inventory")
@@ -42,7 +93,7 @@ function removeStock(){
  	//console.log(inStock);
 
  	var inventory = document.getElementById("inventory");
- 	var newRow = "<tr>" + "<td>" + "<input type='checkbox'>" + "</td>" + "<td>" + materialName + "</td>" + "<td>" + priceName + "</td>" + "<td class=' " + inStock + "'>"; 
+ 	var newRow = "<tr>" + "<td>" + "<input type='checkbox' class='checkbox'>" + "</td>" + "<td>" + materialName + "</td>" + "<td>" + priceName + "</td>" + "<td class=' " + inStock + "'>"; 
  	
  	if (inStock){
  		newRow += "Yes";
@@ -54,7 +105,7 @@ function removeStock(){
  	
 
  	inventory.innerHTML += newRow; 
- 	}
+ }
 
 
 
