c25dbf0666be2410e4390ea9ab2e6774a4b768ac
Ron <ronshafii2@gmail.com>
2016-03-13 11:49:51 -0700

    updating forum and django projects

---
+++ javascript/forum/js/forum2.js
@@ -0,0 +1,70 @@
+//GET XML data from JSON request
+
+
+function get() {	
+	$.getJSON( "http://spreadsheets.google.com/feeds/list/1NlIJLGd32t38kt6mJgc64SFpDM_ltq7nfzaRjV1wpLI/default/public/values?alt=json-in-script&callback=?",
+		function (data) {
+			//inside the results class create a new div with a class called items
+			$('.results').append('<div class="items"></div>');
+			
+			// parse the google feed string and grab each spreadsheet column heading 
+			$.each(data.feed.entry, function(i,entry) {
+				//define the variable "item"
+				var item = '<span style="display:none">' + entry.id.$t + '</span>';
+				item += '<br/>Date: ' + entry.gsx$timestamp.$t;
+				item += '<br/>title: ' + entry.gsx$title.$t;
+				item += '<br/>bodytext: ' + entry.gsx$post.$t;
+				//take the results of all the items and put them in the div class called items
+				$('.items').append('<div>' + item + '</div>');
+
+			});
+	});
+}
+
+
+
+
+//POST XML data
+// define how to submit content to the spreadsheet
+function postToGoogle(title, bodytext) {
+
+	$.ajax({
+	url: "https://docs.google.com/forms/d/1W1jO8DwInFuzlabeYqEp6hUUUqDbR8gESs1Pk6qozOc/formResponse",
+	data: {"entry.1639809944" : title, "entry.1402022725" : bodytext},
+	type: "POST",
+	dataType: "xml",
+	statusCode: {
+		0: function() {
+		get();
+		//Error message
+		},
+
+		200: function() {
+		//Success Message
+		}
+	}
+	});
+}
+
+// define what is being submitted to the function postToGoogle()
+function submit(event){
+	event.preventDefault();
+	var title = document.forms["form"]["title"].value;
+	var bodytext = document.forms["form"]["bodytext"].value;
+
+	postToGoogle(title, bodytext);
+
+
+	//console.log(title, bodytext)
+
+}
+
+$(document).ready(function(){
+	get();
+
+	form = document.getElementById("form")
+
+	form.addEventListener("submit", submit)
+
+});
+
